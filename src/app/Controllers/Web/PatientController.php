e20b3b017db31b4c4989331594550d87c2e3337a
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-15 20:09:32 -0300

    refactor: apply PatientShowAction in controller

--- src/app/Controllers/Web/PatientController.php
+++ src/app/Controllers/Web/PatientController.php
@@ -80,15 +80,8 @@ class PatientController extends BaseController
     public function edit(string $id): string|RedirectResponse
     {
         try {
-            $patient = $this->patientModel->select('
-                    patients.id, patients.image, patients.name, patients.mother_name, patients.birth_date, patients.cpf, patients.cns,
-                    addresses.zip_code, addresses.street, addresses.number, addresses.complement, addresses.neighborhood, addresses.city, addresses.state_id
-                ')
-                ->join('addresses', 'addresses.patient_id = patients.id')
-                ->join('states', 'states.id = addresses.state_id')
-                ->find($id);
-
-            if (!$patient) throw new PatientNotFoundException();
+            $action = Services::patientShowAction();
+            $patient = $action->execute($id);
 
             $states = $this->stateModel->findAll();
 
